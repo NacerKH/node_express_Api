e0e3fda8757baf4705fd2f806868504b51cd59f9
NaserKh <naserK@360medlink.com>
2023-03-01 18:03:20 +0100

    fix env

--- config/db.js
+++ config/db.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-const uri = process.env.MONGODB_URI;
+
+const uri = process.env.URL_MONGO_DB;
+console.log(uri)
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
