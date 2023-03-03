a6322eb971df32dbd49542d7a032c3c193c85dd7
NaserKh <naserK@360medlink.com>
2023-03-03 17:41:00 +0100

    upload file config

---
+++ middlewares/Upload.js
@@ -0,0 +1,12 @@
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads/")
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + "-" + file.originalname)
+    },
+})
+
+exports.upload=multer({ storage: storage }).single('picture')
+
